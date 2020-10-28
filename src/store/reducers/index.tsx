6a84ab0ffdd986a9e76646d65fc92080cd1d02a4
FoxFrontEngineer <artem.bashkatoff@yandex.ru>
2020-10-28 14:55:37 +0000

    Added small bugfix and done for release

--- src/store/reducers/index.tsx
+++ src/store/reducers/index.tsx
@@ -12,7 +12,7 @@ const initialState = {
 
 function rootReducer(state = initialState, action: RadioButtonActionTypes) {
   if (action.type === CHECK_PAYMENT_TYPE) {
-    return { paymentType: action.payload };
+    return { ...state, paymentType: action.payload };
   }
   if (action.type === SWITCH_NDFL_TOGGLE) {
     return { ...state, ndflToggleStatus: !state.ndflToggleStatus };
